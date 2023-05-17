f9fea5f60a8a5bf14852f89892eb671c4ee06ed3
Edmond Dusa <edmond.dusa@gmail.com>
2023-05-17 15:49:13 +0200

    Added ExchangeRate enity types

---
+++ src/main/java/com/betvictor/currency/types/exchangerate/Motd.java
@@ -0,0 +1,6 @@
+package com.betvictor.currency.types.exchangerate;
+
+public class Motd {
+    public String msg;
+    public String url;
+}
---
+++ src/main/java/com/betvictor/currency/types/exchangerate/Response.java
@@ -0,0 +1,9 @@
+package com.betvictor.currency.types.exchangerate;
+
+public class Response {
+    public Motd motd;
+    public boolean success;
+    public String base;
+    public String date;
+    public Rates rates;
+}

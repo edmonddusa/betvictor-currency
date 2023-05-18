a49e7a8e0fd1b301c3faca9bf8ef27616d56b73d
Edmond Dusa <edmond.dusa@gmail.com>
2023-05-18 19:02:50 +0200

    Quick boot fix

--- src/main/java/com/betvictor/currency/CurrencyApplication.java
+++ src/main/java/com/betvictor/currency/CurrencyApplication.java
@@ -1,19 +1,21 @@
 package com.betvictor.currency;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* 
 import com.betvictor.currency.entities.ExchangeRates;
 import com.betvictor.currency.service.LoaderService;
 import com.betvictor.currency.service.LoaderService.CurrencySource;
+*/
 
 @SpringBootApplication
 public class CurrencyApplication implements CommandLineRunner {
 
-	@Autowired
-    private LoaderService loaderService;
+	//@Autowired
+    //private LoaderService loaderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyApplication.class, args);
@@ -22,9 +24,6 @@ public class CurrencyApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//ExchangeRates rates = loaderService.loadRates(CurrencySource.OPENEXCHANGERATES, "EUR");
-		
-		
-		
 	}
 
 }

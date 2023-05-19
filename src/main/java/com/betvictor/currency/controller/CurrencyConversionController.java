f9b479b183217ef95a9ccc8fcce6cccce11864ce
Edmond Dusa <edmond.dusa@gmail.com>
2023-05-19 10:03:31 +0200

    Added REST Controller

---
+++ src/main/java/com/betvictor/currency/controller/CurrencyConversionController.java
@@ -0,0 +1,22 @@
+package com.betvictor.currency.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.betvictor.currency.entity.ExchangeRates;
+import com.betvictor.currency.service.CurrencyConversionService;
+
+@RestController
+public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyConversionService conversionService;
+
+    @RequestMapping(value = "/rates/{baseSymbol}", method = RequestMethod.GET)
+    public ExchangeRates getFoosBySimplePathWithPathVariable(@PathVariable("baseSymbol") String baseSymbol) {
+        return conversionService.getExchangeRates(baseSymbol);
+    }
+}

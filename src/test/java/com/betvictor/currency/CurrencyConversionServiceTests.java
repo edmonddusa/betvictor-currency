ccd0de5ce88dc940ba560694ffd9a608f36cd5e9
Edmond Dusa <edmond.dusa@gmail.com>
2023-05-19 09:32:47 +0200

    Added test for currency conversion

---
+++ src/test/java/com/betvictor/currency/CurrencyConversionServiceTests.java
@@ -0,0 +1,35 @@
+package com.betvictor.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.betvictor.currency.entity.ExchangeRates;
+import com.betvictor.currency.service.CurrencyConversionService;
+
+
+@SpringBootTest
+class CurrencyConversionServiceTests {
+
+	@Autowired
+    private CurrencyConversionService conversionService;
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(conversionService).isNotNull();
+	}
+
+	@Test
+	void testCache() {
+		ExchangeRates rates0 = conversionService.getExchangeRates("EUR");
+		ExchangeRates rates1 = conversionService.getExchangeRates("EUR");
+
+
+		assertThat(rates0.getTimestamp()).isEqualTo(rates1.getTimestamp());
+		
+	}
+
+	
+}
